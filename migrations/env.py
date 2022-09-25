13bcea0b3a3384a66f700fe5cd1dbfde4f53dfcf
addcodeapp <addcodeapp@gmail.com>
2022-09-24 22:12:21 -0500

    Modificando BD

--- migrations/env.py
+++ migrations/env.py
@@ -6,27 +6,14 @@ from sqlalchemy import pool
 from alembic import context
 
 from core.config import settings
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
 
+config = context.config
 config.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
 from app.db.models import Base
 target_metadata = Base.metadata
-#target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- migrations/versions/913637352f96_crear_modelos.py
+++ migrations/versions/913637352f96_crear_modelos.py
@@ -1,8 +1,8 @@
-"""Crear modelos iniciales
+"""Crear modelos
 
-Revision ID: 96bfe2efef8c
+Revision ID: 913637352f96
 Revises: 
-Create Date: 2022-09-10 18:35:44.995522
+Create Date: 2022-09-24 22:06:13.510775
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '96bfe2efef8c'
+revision = '913637352f96'
 down_revision = None
 branch_labels = None
 depends_on = None
